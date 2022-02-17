8b0fdc0533b5a5af55f19195aa4be47393ad09ac
yameriong <hojunchoi@hotmail.co.kr>
2022-02-18 00:32:58 +0900

    Feat Add First Commit

---
+++ src/main/java/com/chois/reactive/ChoisReactiveApplication.java
@@ -0,0 +1,13 @@
+package com.chois.reactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChoisReactiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChoisReactiveApplication.class, args);
+    }
+
+}
